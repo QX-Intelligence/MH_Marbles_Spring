4a5521e49e13aa596ec4966cfcfa2bb24efa5ad5
akhil <malisettybhargava25@gmail.com>
2026-04-19 17:38:53 +0530

    added spring

--- src/main/java/com/qi/controller/AdminController.java
+++ src/main/java/com/qi/controller/AdminController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/spring")
 public class AdminController {
     private final UsersService usersService;
-    @GetMapping("/owner/info")
+    @GetMapping("/spring/owner/info")
     @PreAuthorize("hasRole('OWNER')")
     public ResponseEntity<OwnerInfo> getOwnerInfo(Authentication authentication){
         return new ResponseEntity<>(usersService.getOwnerInfo(authentication), HttpStatus.OK);
